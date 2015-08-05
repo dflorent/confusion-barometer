e540b23661980ad843c3572caa5b3e65d32bf2a1
Florent Desjardins <florent.desjardins@gmail.com>
2015-08-05 14:37:49 +0200

    add template engine and setup socket.io

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var app = express();
+var nunjucks = require('nunjucks');
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res) {
-  res.send('hi');
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
 });
 
-app.listen(app.get('port'), function() {
-  console.log(process.env.PORT);
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+var server = app.listen(app.get('port'), function() {
   console.log('server running http://127.0.0.1:%d', app.get('port'));
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  socket.emit('foo');
 });
