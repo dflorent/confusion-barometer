6f33679f504de10aa9b8c8940483114e8b180656
Florent Desjardins <florent.desjardins@gmail.com>
2015-10-30 12:22:28 +0100

    writing tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,25 @@
 process.env.NODE_ENV = 'test';
 
-describe('homepage', function() {
-  it('should respond with 200 status');
+var Browser = require('zombie');
+
+Browser.localhost('127.0.0.1', 5000);
+
+describe('User visits homepage', function() {
+  var browser = new Browser();
+
+  before(function(done) {
+    browser.visit('/', done);
+  });
+
+  it('Should respond with success', function() {
+    browser.assert.success();
+  });
+
+  it('Should have a start button', function() {
+    browser.assert.element('#start');
+  });
+
+  it('Should have a start button disabled', function() {
+    browser.assert.hasClass('#start', 'disabled');
+  });
 });
